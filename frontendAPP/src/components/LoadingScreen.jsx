a10b96d6498d94f679fcc14ade9f9b8145faeb72
Marco <marcofiguero3@gmail.com>
2025-03-16 19:10:29 -0300

    cambios en loading

--- frontendAPP/src/components/LoadingScreen.jsx
+++ frontendAPP/src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const LoadingScreen = ({ onComplete }) => {
 
     const [text, setText] = useState("");
-    const fullText = "CIMA - Centro de Integracion";
+    const fullText = "CIMA - Centro de Rehabilitacion e Integracion Escolar";
 
     useEffect(() => {
         let index = 0;
