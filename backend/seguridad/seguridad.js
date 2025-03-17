c82162eb0d7db024b1ca74482f0089b320489ac5
Marco <marcofiguero3@gmail.com>
2025-03-17 12:46:45 -0300

    cambiada bycypt libarary

--- backend/seguridad/seguridad.js
+++ backend/seguridad/seguridad.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt"); // Para encriptar contraseñas
+const bcrypt = require("bcryptjs"); // Para encriptar contraseñas
 const auth = require("./auth");
 require('dotenv').config();
 
