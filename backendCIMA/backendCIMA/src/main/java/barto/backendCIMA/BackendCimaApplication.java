3908a527c9d9b4a10a6ca14fd8d7bfb0f5841723
Marco <marcofiguero3@gmail.com>
2025-04-01 11:12:46 -0300

    java

---
+++ backendCIMA/backendCIMA/src/main/java/barto/backendCIMA/BackendCimaApplication.java
@@ -0,0 +1,13 @@
+package barto.backendCIMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCimaApplication.class, args);
+	}
+
+}
---
+++ backendCIMA/backendCIMA/src/test/java/barto/backendCIMA/BackendCimaApplicationTests.java
@@ -0,0 +1,13 @@
+package barto.backendCIMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
